08d39ef799e83f702f2cb30d93fbbdae82dd3743
Samuel <87200735+Gustixa@users.noreply.github.com>
2023-10-18 15:09:54 -0600

    Commit

--- main.cpp
+++ main.cpp
@@ -44,46 +44,61 @@ int main(int argc, char* argv[]) {
 	scene.objects["Mercury"     ] = Mesh::readObj("./resources/planet.obj");
 	scene.objects["Venus"       ] = Mesh::readObj("./resources/planet.obj");
 	scene.objects["Earth"       ] = Mesh::readObj("./resources/planet.obj");
-	scene.objects["Moon"        ] = Mesh::readObj("./resources/moon.obj");
+	scene.objects["Earth Clouds"] = Mesh::readObj("./resources/planet.obj");
+	scene.objects["Moon"        ] = Mesh::readObj("./resources/moon.obj"  );
 	scene.objects["Mars"        ] = Mesh::readObj("./resources/planet.obj");
 	scene.objects["Jupiter"     ] = Mesh::readObj("./resources/planet.obj");
 	scene.objects["Saturn"      ] = Mesh::readObj("./resources/planet.obj");
-	scene.objects["Saturn Rings"] = Mesh::readObj("./resources/rings.obj");
-	scene.objects["Uranus :)"   ] = Mesh::readObj("./resources/planet.obj");
+	scene.objects["Saturn Rings"] = Mesh::readObj("./resources/rings.obj" );
+	scene.objects["Uranus"      ] = Mesh::readObj("./resources/planet.obj");
 	scene.objects["Neptune"     ] = Mesh::readObj("./resources/planet.obj");
-	scene.objects["Pluto :("    ] = Mesh::readObj("./resources/moon.obj");
+	scene.objects["Pluto"       ] = Mesh::readObj("./resources/moon.obj"  );
 
 	// Load.png
+	scene.textures["Sun"         ] = loadImage("./resources/Sun.png"    );
+	scene.textures["Mercury"     ] = loadImage("./resources/Mercury.png");
+	scene.textures["Venus"       ] = loadImage("./resources/Venus.png"  );
+	scene.textures["Earth"       ] = loadImage("./resources/Earth.png"  );
+	scene.textures["Earth Clouds"] = loadImage("./resources/Clouds.png" );
+	scene.textures["Moon"        ] = loadImage("./resources/Moon.png"   );
+	scene.textures["Mars"        ] = loadImage("./resources/Mars.png"   );
+	scene.textures["Jupiter"     ] = loadImage("./resources/Jupiter.png");
+	scene.textures["Saturn"      ] = loadImage("./resources/Saturn.png" );
+	scene.textures["Saturn Rings"] = loadImage("./resources/Rings.png"  );
+	scene.textures["Uranus"      ] = loadImage("./resources/Uranus.png" );
+	scene.textures["Neptune"     ] = loadImage("./resources/Neptune.png");
+	scene.textures["Pluto"       ] = loadImage("./resources/Mercury.png");
 
-//	scene.textures["Sun"] = loadImage("./resources/sun.png");
 	
 	// Position
 	scene.objects["Sun"         ].position = vec3(0);
 	scene.objects["Mercury"     ].position = vec3(39.00, 0, 0);
 	scene.objects["Venus"       ].position = vec3(72.00, 0, 0);
 	scene.objects["Earth"       ].position = vec3(100.0, 0, 0);
+	scene.objects["Earth Clouds"].position = vec3(100.0, 0, 0);
 	scene.objects["Moon"        ].position = vec3(110.0, 0, 0);
 	scene.objects["Mars"        ].position = vec3(152.0, 0, 0);
 	scene.objects["Jupiter"     ].position = vec3(520.0, 0, 0);
 	scene.objects["Saturn"      ].position = vec3(954.0, 0, 0);
 	scene.objects["Saturn Rings"].position = vec3(954.0, 0, 0);
-	scene.objects["Uranus :)"   ].position = vec3(1920., 0, 0);
+	scene.objects["Uranus"      ].position = vec3(1920., 0, 0);
 	scene.objects["Neptune"     ].position = vec3(3006., 0, 0);
-	scene.objects["Pluto :("    ].position = vec3(3900., 0, 0);
+	scene.objects["Pluto"       ].position = vec3(3900., 0, 0);
 
 	// Scale
 	scene.objects["Sun"         ].scale = vec3(13.910);
 	scene.objects["Mercury"     ].scale = vec3(0.2240);
 	scene.objects["Venus"       ].scale = vec3(0.6052);
 	scene.objects["Earth"       ].scale = vec3(0.6371);
+	scene.objects["Earth Clouds"].scale = vec3(0.6571);
 	scene.objects["Moon"        ].scale = vec3(0.1740);
 	scene.objects["Mars"        ].scale = vec3(0.3390);
 	scene.objects["Jupiter"     ].scale = vec3(6.9910);
 	scene.objects["Saturn"      ].scale = vec3(5.8232);
 	scene.objects["Saturn Rings"].scale = vec3(5.8232);
-	scene.objects["Uranus :)"   ].scale = vec3(2.5362);
+	scene.objects["Uranus"      ].scale = vec3(2.5362);
 	scene.objects["Neptune"     ].scale = vec3(2.4622);
-	scene.objects["Pluto :("    ].scale = vec3(0.0188);
+	scene.objects["Pluto"       ].scale = vec3(0.0188);
 
 	while (running) {
 		SDL_Event event;
@@ -165,58 +180,62 @@ void render(Scene& scene) {
 	if (input[KEY_Q]) scene.camera.move_camera(0.0, 0.0, -1.0, moveSensitivity * delta_time);
 	if (input[KEY_R]) {
 		scene.camera.position = vec3(0, 4500, 0);
-		scene.objects["Sun"].position = vec3(0);
-		scene.objects["Mercury"].position = vec3(39.00, 0, 0);
-		scene.objects["Venus"].position = vec3(72.00, 0, 0);
-		scene.objects["Earth"].position = vec3(100.0, 0, 0);
-		scene.objects["Moon"].position = vec3(110.0, 0, 0);
-		scene.objects["Mars"].position = vec3(152.0, 0, 0);
-		scene.objects["Jupiter"].position = vec3(520.0, 0, 0);
-		scene.objects["Saturn"].position = vec3(954.0, 0, 0);
+		scene.objects["Sun"         ].position = vec3(0);
+		scene.objects["Mercury"     ].position = vec3(39.00, 0, 0);
+		scene.objects["Venus"       ].position = vec3(72.00, 0, 0);
+		scene.objects["Earth"       ].position = vec3(100.0, 0, 0);
+		scene.objects["Earth Clouds"].position = vec3(100.0, 0, 0);
+		scene.objects["Moon"        ].position = vec3(110.0, 0, 0);
+		scene.objects["Mars"        ].position = vec3(152.0, 0, 0);
+		scene.objects["Jupiter"     ].position = vec3(520.0, 0, 0);
+		scene.objects["Saturn"      ].position = vec3(954.0, 0, 0);
 		scene.objects["Saturn Rings"].position = vec3(954.0, 0, 0);
-		scene.objects["Uranus :)"].position = vec3(1920., 0, 0);
-		scene.objects["Neptune"].position = vec3(3006., 0, 0);
-		scene.objects["Pluto :("].position = vec3(3900., 0, 0);
-		scene.objects["Sun"].rotation = vec3(0);
-		scene.objects["Mercury"].rotation = vec3(0);
-		scene.objects["Venus"].rotation = vec3(0);
-		scene.objects["Earth"].rotation = vec3(0);
-		scene.objects["Moon"].rotation = vec3(0);
-		scene.objects["Mars"].rotation = vec3(0);
-		scene.objects["Jupiter"].rotation = vec3(0);
-		scene.objects["Saturn"].rotation = vec3(0);
+		scene.objects["Uranus"      ].position = vec3(1920., 0, 0);
+		scene.objects["Neptune"     ].position = vec3(3006., 0, 0);
+		scene.objects["Pluto"       ].position = vec3(3900., 0, 0);
+
+		scene.objects["Sun"         ].rotation = vec3(0);
+		scene.objects["Mercury"     ].rotation = vec3(0);
+		scene.objects["Venus"       ].rotation = vec3(0);
+		scene.objects["Earth"       ].rotation = vec3(0);
+		scene.objects["Earth Clouds"].rotation = vec3(0);
+		scene.objects["Moon"        ].rotation = vec3(0);
+		scene.objects["Mars"        ].rotation = vec3(0);
+		scene.objects["Jupiter"     ].rotation = vec3(0);
+		scene.objects["Saturn"      ].rotation = vec3(0);
 		scene.objects["Saturn Rings"].rotation = vec3(0);
-		scene.objects["Uranus :)"].rotation = vec3(0);
-		scene.objects["Neptune"].rotation = vec3(0);
-		scene.objects["Pluto :("].rotation = vec3(0);
+		scene.objects["Uranus"      ].rotation = vec3(0);
+		scene.objects["Neptune"     ].rotation = vec3(0);
+		scene.objects["Pluto"       ].rotation = vec3(0);
 	}
 
 	// Scene Tick Update ----------------------------------------------------------------------------------------------------------
-	scene.camera.process(scene.RESX, scene.RESY);
 		// Planets
 	if (!pause) {
 		rotateAroundAnchor(scene.objects["Mercury"     ].position, scene.objects["Sun"].position, vec3(0, 1.0 / 0.8797 * delta_time, 0));
 		rotateAroundAnchor(scene.objects["Venus"       ].position, scene.objects["Sun"].position, vec3(0, 1.0 / 2.2470 * delta_time, 0));
 		rotateAroundAnchor(scene.objects["Earth"       ].position, scene.objects["Sun"].position, vec3(0, 1.0 / 3.6525 * delta_time, 0));
+		rotateAroundAnchor(scene.objects["Earth Clouds"].position, scene.objects["Sun"].position, vec3(0, 1.0 / 3.6525 * delta_time, 0));
 		rotateAroundAnchor(scene.objects["Moon"        ].position, scene.objects["Sun"].position, vec3(0, 1.0 / 3.6525 * delta_time, 0));
 		rotateAroundAnchor(scene.objects["Mars"        ].position, scene.objects["Sun"].position, vec3(0, 1.0 / 6.8693 * delta_time, 0));
 		rotateAroundAnchor(scene.objects["Jupiter"     ].position, scene.objects["Sun"].position, vec3(0, 1.0 / 4.3330 * delta_time, 0));
 		rotateAroundAnchor(scene.objects["Saturn"      ].position, scene.objects["Sun"].position, vec3(0, 1.0 / 10.759 * delta_time, 0));
 		rotateAroundAnchor(scene.objects["Saturn Rings"].position, scene.objects["Sun"].position, vec3(0, 1.0 / 10.759 * delta_time, 0));
-		rotateAroundAnchor(scene.objects["Uranus :)"   ].position, scene.objects["Sun"].position, vec3(0, 1.0 / 30.687 * delta_time, 0));
+		rotateAroundAnchor(scene.objects["Uranus"      ].position, scene.objects["Sun"].position, vec3(0, 1.0 / 30.687 * delta_time, 0));
 		rotateAroundAnchor(scene.objects["Neptune"     ].position, scene.objects["Sun"].position, vec3(0, 1.0 / 60.190 * delta_time, 0));
-		rotateAroundAnchor(scene.objects["Pluto :("    ].position, scene.objects["Sun"].position, vec3(0, 1.0 / 90.582 * delta_time, 0));
-		// PLanet Rotations
-		scene.objects["Sun"         ].rotation += vec3(63.84 * delta_time, 0, 0);
+		rotateAroundAnchor(scene.objects["Pluto"       ].position, scene.objects["Sun"].position, vec3(0, 1.0 / 90.582 * delta_time, 0));
+		// Planet Rotations
+		scene.objects["Sun"         ].rotation += vec3( 63.84 * delta_time, 0, 0);
 		scene.objects["Mercury"     ].rotation += vec3(-422.4 * delta_time, 0, 0);
-		scene.objects["Venus"       ].rotation += vec3(580.8 * delta_time, 0, 0);
+		scene.objects["Venus"       ].rotation += vec3( 580.8 * delta_time, 0, 0);
 		scene.objects["Earth"       ].rotation += vec3(-24.00 * delta_time, 0, 0);
+		scene.objects["Earth Clouds"].rotation += vec3(-24.00 * delta_time, 0, 0);
 		scene.objects["Mars"        ].rotation += vec3(-24.39 * delta_time, 0, 0);
 		scene.objects["Jupiter"     ].rotation += vec3(-10.00 * delta_time, 0, 0);
 		scene.objects["Saturn"      ].rotation += vec3(-10.39 * delta_time, 0, 0);
-		scene.objects["Uranus :)"   ].rotation += vec3(0, -17.14 * delta_time, 0);
+		scene.objects["Uranus"      ].rotation += vec3(0, -17.14 * delta_time, 0);
 		scene.objects["Neptune"     ].rotation += vec3(-16.00 * delta_time, 0, 0);
-		scene.objects["Pluto :("    ].rotation += vec3(-24.00 * delta_time, 0, 0);
+		scene.objects["Pluto"       ].rotation += vec3(-24.00 * delta_time, 0, 0);
 
 		scene.objects["Saturn Rings"].rotation += vec3(15.00 * delta_time, 5.0 * delta_time, 0);
 		// Moons
@@ -227,6 +246,7 @@ void render(Scene& scene) {
 			scene.camera.position.x = scene_loop->second.position.x;
 			scene.camera.position.z = scene_loop->second.position.z;
 		}
+		scene.camera.process(scene.RESX, scene.RESY);
 	}
 
 	// Vertex Shader
@@ -239,14 +259,15 @@ void render(Scene& scene) {
 	renderMesh(renderer, scene, scene.objects["Mercury"     ], vec3(1.0, 1.0, 1.0), Shader::SUN, scene.textures["Mercury"     ]);
 	renderMesh(renderer, scene, scene.objects["Venus"       ], vec3(1.0, 1.0, 1.0), Shader::SUN, scene.textures["Venus"       ]);
 	renderMesh(renderer, scene, scene.objects["Earth"       ], vec3(1.0, 1.0, 1.0), Shader::SUN, scene.textures["Earth"       ]);
+	renderMesh(renderer, scene, scene.objects["Earth Clouds"], vec3(1.0, 1.0, 1.0), Shader::SUN, scene.textures["Earth Clouds"]);
 	renderMesh(renderer, scene, scene.objects["Moon"        ], vec3(1.0, 1.0, 1.0), Shader::SUN, scene.textures["Moon"        ]);
 	renderMesh(renderer, scene, scene.objects["Mars"        ], vec3(1.0, 1.0, 1.0), Shader::SUN, scene.textures["Mars"        ]);
 	renderMesh(renderer, scene, scene.objects["Jupiter"     ], vec3(1.0, 1.0, 1.0), Shader::SUN, scene.textures["Jupiter"     ]);
 	renderMesh(renderer, scene, scene.objects["Saturn"      ], vec3(1.0, 1.0, 1.0), Shader::SUN, scene.textures["Saturn"      ]);
 	renderMesh(renderer, scene, scene.objects["Saturn Rings"], vec3(1.0, 1.0, 1.0), Shader::SUN, scene.textures["Saturn Rings"]);
-	renderMesh(renderer, scene, scene.objects["Uranus :)"   ], vec3(1.0, 1.0, 1.0), Shader::SUN, scene.textures["Uranus :)"   ]);
+	renderMesh(renderer, scene, scene.objects["Uranus"      ], vec3(1.0, 1.0, 1.0), Shader::SUN, scene.textures["Uranus"      ]);
 	renderMesh(renderer, scene, scene.objects["Neptune"     ], vec3(1.0, 1.0, 1.0), Shader::SUN, scene.textures["Neptune"     ]);
-	renderMesh(renderer, scene, scene.objects["Pluto :("    ], vec3(1.0, 1.0, 1.0), Shader::SUN, scene.textures["Pluto :("    ]);
+	renderMesh(renderer, scene, scene.objects["Pluto"       ], vec3(1.0, 1.0, 1.0), Shader::SUN, scene.textures["Pluto"       ]);
 
 	SDL_RenderPresent(renderer);
 }
